cf7ae6bb82223dd03d09cb48701f1dcfd6abd923
Brandon CS Sanders <github.b@thesanders.us>
2013-06-09 13:28:18 -0700

    strong_parameters

---
+++ strong_parameters.rb
@@ -0,0 +1,15 @@
+# strong_parameters
+gem 'strong_parameters'
+run 'bundle install'
+git add: "."
+git commit: %Q{ -m 'install strong_parameters' }
+
+
+initializer 'strong_parameters.rb', 'ActiveRecord::Base.send(:include, ActiveModel::ForbiddenAttributesProtection)'
+git add: "."
+git commit: %Q{ -m 'activate strong_parameters on all ActiveRecord models' }
+
+
+gsub_file 'config/application.rb', /config.active_record.whitelist_attributes = true/, 'config.active_record.whitelist_attributes = false'
+git add: "."
+git commit: %Q{ -m 'disable the default rails 3.2 whitelisting of parameters' }
