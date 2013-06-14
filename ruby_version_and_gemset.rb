ccb8ec39d50688e88f1e57dd75282a9757429f0b
Brandon CS Sanders <github.b@thesanders.us>
2013-06-14 16:12:32 -0700

    remove duplicate

--- ruby_version_and_gemset.rb
+++
@@ -1,6 +0,0 @@
-create_file '.ruby_version', "1.9.3-p429"
-create_file '.ruby_gemset', app_name
-
-git add: '.ruby_version'
-git add: '.ruby_gemset'
-git commit: %Q{ -m '.ruby_version and .ruby_gemset' }
