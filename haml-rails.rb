088bf8d7b009b555872a0d8f67e0d1d2fd93b5aa
Brandon CS Sanders <github.b@thesanders.us>
2013-08-17 10:15:51 -0700

    For heroku haml-rails must work in production

--- haml-rails.rb
+++ haml-rails.rb
@@ -1,4 +1,4 @@
 # haml
 require File.expand_path('../gem_install_bundle_and_commit', __FILE__)
 
-gem_install_bundle_and_commit 'haml-rails', '>= 0.3.4', :group => :development
+gem_install_bundle_and_commit 'haml-rails', '>= 0.3.4'
