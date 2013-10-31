912bb7629e727a91e7f224b149148ab5d606bb16
Brandon CS Sanders <github.b@thesanders.us>
2013-10-31 07:46:51 -0700

    ruby version works with new Ruby-BLAH format

--- ruby-version-in-gemfile.rb
+++ ruby-version-in-gemfile.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../gem_install_bundle_and_commit', __FILE__)
 
-if IO.read('.ruby-version') =~ /^(ruby-)?(\d+\.\d+.\d+)(-p\d+)?$/
+if IO.read('.ruby-version') =~ /^([Rr]uby[- ])?(\d+\.\d+.\d+)(-p\d+)?$/
   version = $2
   inject_into_file "Gemfile", "ruby '#{version}'\n", :after => "source 'https://rubygems.org'\n"
   git add: "Gemfile"
