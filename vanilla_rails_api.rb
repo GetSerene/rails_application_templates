50781e38b200ece1522b87f1598126d7eaae5278
Brandon CS Sanders <brandon@thesanders.us>
2014-04-09 16:00:06 -0700

    vanilla_rails_api

---
+++ vanilla_rails_api.rb
@@ -0,0 +1,4 @@
+# vanilla rails
+# run 'bundle install' # moved out into the recipe in the README because rake doesn't work until after bundle install
+git add: "."
+git commit: %Q{ -m 'install vanilla rails-api' }
