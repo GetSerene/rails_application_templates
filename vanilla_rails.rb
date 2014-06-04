83c5444a7a8fba11707e7daa4e369a792d18aa57
Brandon CS Sanders <brandon@thesanders.us>
2014-06-04 13:23:33 -0700

    change message

--- vanilla_rails.rb
+++ vanilla_rails.rb
@@ -1,4 +1,4 @@
 # vanilla rails
 # run 'bundle install' # moved out into the recipe in the README because rake doesn't work until after bundle install
 git add: "."
-git commit: %Q{ -m 'install vanilla rails' }
+git commit: %Q{ -m 'rails new ... vanilla' }
