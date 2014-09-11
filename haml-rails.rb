b84cd9f95f6301b1029e909153baeaca7058d68c
Brandon CS Sanders <brandon@thesanders.us>
2014-09-11 09:59:22 -0700

    haml-rails … config the generators

--- haml-rails.rb
+++ haml-rails.rb
@@ -2,3 +2,9 @@
 require File.expand_path('../lib/gem_install_bundle_and_commit', __FILE__)
 
 gem_install_bundle_and_commit 'haml-rails', '>= 0.5.3'
+
+inject_into_file "config/environments/development.rb", <<-HERE
+  config.generators do |g|
+    g.template_engine :haml
+  end
+HERE , before: 'end'
