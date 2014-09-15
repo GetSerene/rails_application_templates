c0cca20009b5f7cbc452f406ab100e820f6911b8
Brandon CS Sanders <brandon@thesanders.us>
2014-09-15 14:49:03 -0700

    improve haml-rails

--- haml-rails.rb
+++ haml-rails.rb
@@ -3,8 +3,11 @@ require File.expand_path('../lib/gem_install_bundle_and_commit', __FILE__)
 
 gem_install_bundle_and_commit 'haml-rails', '>= 0.5.3'
 
-inject_into_file "config/environments/development.rb", <<-HERE
+inject_into_file "config/environments/development.rb", %{
   config.generators do |g|
     g.template_engine :haml
   end
-HERE , before: 'end'
+}, before: /end$/m
+
+git add: "config/environments/development.rb"
+git commit: %Q{ -m 'make haml the default for generators' }
