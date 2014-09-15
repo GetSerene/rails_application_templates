78319186d2c5b071097a42a89fb969086dafaaea
Brandon CS Sanders <brandon@thesanders.us>
2014-09-15 14:48:27 -0700

    swap ruby-version-in-gemfile to 2.1.2

--- ruby-version-in-gemfile.rb
+++ ruby-version-in-gemfile.rb
@@ -3,7 +3,7 @@ require File.expand_path('../lib/gem_install_bundle_and_commit', __FILE__)
 if File.exist?('.ruby-version') && IO.read('.ruby-version') =~ /^([Rr]uby[- ])?(\d+\.\d+.\d+)(-p\d+)?$/
   version = $2
 else
-  version = "2.1.1"
+  version = "2.1.2"
 end
 
 inject_into_file "Gemfile", "ruby '#{version}'\n", :after => "source 'https://rubygems.org'\n"
