6deced679d9b22b03a5454bff44295086425a8d7
Brandon CS Sanders <brandon@thesanders.us>
2014-12-03 15:45:59 -0800

    fix heroku for rails 4

--- heroku.rb
+++ heroku.rb
@@ -1,3 +1,4 @@
 require File.expand_path('../lib/gem_install_bundle_and_commit', __FILE__)
 
 gem_install_bundle_and_commit 'rails_12factor', :groups => [:production]
+gem_install_bundle_and_commit 'rails_serve_static_assets'
