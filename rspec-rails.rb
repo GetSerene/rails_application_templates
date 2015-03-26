18e37da8989fc3d6516b5a16edeb1684959bf56c
Brandon CS Sanders <brandon@thesanders.us>
2015-03-26 16:36:11 -0700

    upgrade rspec-rails

--- rspec-rails.rb
+++ rspec-rails.rb
@@ -1,7 +1,7 @@
 # rspec-rails
 require File.expand_path('../lib/gem_install_bundle_and_commit', __FILE__)
 
-gem_install_bundle_and_commit 'rspec-rails', '~> 3.1.0', groups: [:development, :test]
+gem_install_bundle_and_commit 'rspec-rails', '~> 3.2.0', groups: [:development, :test]
 
 generate 'rspec:install'
 git add: "."
