3d69388a053ed987f43666fcf257b359d226983e
Brandon CS Sanders <brandon@thesanders.us>
2015-09-11 21:01:42 -0700

    update devise and output the views

--- devise-for-user.rb
+++ devise-for-user.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../lib/gem_install_bundle_and_commit', __FILE__)
 
-gem_install_bundle_and_commit 'devise', git: 'https://github.com/plataformatec/devise.git', :branch => 'lm-rails-4-2'
+gem_install_bundle_and_commit 'devise'
 generate 'devise:install'
 git add: 'config/initializers/devise.rb'
 git add: 'config/locales/devise.en.yml'
@@ -16,3 +16,7 @@ git commit: %Q{ -m 'rails generate devise User' }
 rake 'db:migrate'
 git add: 'db/schema.rb'
 git commit: %Q{ -m 'rake db:migrate for devise migration' }
+
+generate 'devise:views'
+git add: '.'
+git commit: %Q{ -m 'rake generate devise:views' }
