a1245a6135c7c93b4f34e4cb9c327dfbbd69f4b5
Brandon CS Sanders <brandon@thesanders.us>
2015-09-11 21:02:11 -0700

    include links for devise in bootstrap

--- bootstrap-sass.rb
+++ bootstrap-sass.rb
@@ -15,3 +15,12 @@ remove_file 'app/views/layouts/_navigation_links.html.erb'
 create_file 'app/views/layouts/_navigation_links.html.haml', '-# add navigation links to this file'
 git add: 'app/views/layouts/*.html.haml'
 git commit: %Q{ -m 'rails generate layout:install bootstrap3' }
+
+generate 'layout:navigation'
+git add: '.'
+git commit: %Q{-m 'rails generate layout:navigation'}
+
+generate 'layout:devise bootstrap3'
+git add: '.'
+git commit: %Q{-m 'rails generate layout:devise bootstrap3'}
+
